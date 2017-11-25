effd20b800948b26233b051c647ad7484f574514
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2017-11-25 14:38:55 +0900

    restart mysql

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ for HOST in isu1 isu2 isu3; do
   ssh $HOST "source ~/.profile; /home/isucon/deploy.sh $CURRENT_BRANCH"
 done
 
+ssh isu4 "source ~/.profile; sudo systemctl restart mysql"
+
 curl -X POST -d "payload={\"text\": \"deploy $CURRENT_BRANCH\"}" https://hooks.slack.com/services/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
