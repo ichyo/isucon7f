b076590583a606fed39289abca1dad727e6bb812
KINOSHITA Minoru <kinoshita.minoru22@gmail.com>
2017-11-25 12:41:32 +0900

    notify

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 for HOST in isu1 isu2 isu3; do
   ssh $HOST "source ~/.profile; /home/isucon/deploy.sh $CURRENT_BRANCH"
 done
+
+curl -X POST -d "payload={\"text\": \"deploy $CURRENT_BRANCH\"}" https://hooks.slack.com/services/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
